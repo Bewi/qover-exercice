ee8eb8df53f160a0ef9c9abddbf94ebf2590d012
bwilfart <benjamin.wilfart@staff.voo.be>
2017-06-07 09:37:06 +0200

    Init packaging, app and server

---
+++ app/AppComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'React';
+
+class AppComponent extends Component {
+
+    render() {
+        return <h1>Hello world</h1>;
+    }
+
+}
+
+export default AppComponent;
